364012043932e7ade655d7a2a47c7d61eb9d35ff
brankalazic <brankala28@gmail.com>
2020-12-26 17:33:17 +0100

    Create the main menu component - static

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Card, Container } from 'react-bootstrap';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { MainMenu } from '../MainMenu/MainMenu';
 
 function App() {
   return (
