808a00be795ab36c3495321818ae22bbe53d45ca
brankalazic <brankala28@gmail.com>
2020-12-26 14:00:13 +0100

    Project cleanup

---
+++ src/components/MainMenu/MainMenu.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Container, Nav } from 'react-bootstrap';
+
+export class MainMenuItem {
+    text: string = '';
+    link: string = '#';
+
+    constructor(text: string, link: string) {
+        this.text = text;
+        this.link = link;
+    }
+}
+
+interface MainMenuProperties {
+    items: MainMenuItem[];
+}
+
+export class MainMenu extends React.Component<MainMenuProperties> {
+    render() {
+        return (
+            <Container>
+                <Nav variant="tabs">
+                    { this.props.items.map(item => {
+                        return (
+                            <Nav.Link href= { item.link}>
+                                {item.text}
+                            </Nav.Link>
+                        );
+                    })}
+                </Nav>
+            </Container>
+        );
+    }
+
+    private makeNavLink(item: MainMenuItem) {
+        return (
+            <Nav.Link href= { item.link}>
+                {item.text}
+            </Nav.Link>
+        )
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App/App';
+import { MainMenu, MainMenuItem } from './components/MainMenu/MainMenu';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const menuItems = [
+  new MainMenuItem("Home", "/"),
+  new MainMenuItem("Contact", "/contact/"),
+  new MainMenuItem("LogIn", "/login/"),
+]
 
 ReactDOM.render(
   <React.StrictMode>
+    <MainMenu items= { menuItems }></MainMenu>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
