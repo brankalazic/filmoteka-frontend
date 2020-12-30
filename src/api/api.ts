f8f65dbecaf9129291296c617a83454a1e61f3bb
brankalazic <brankala28@gmail.com>
2020-12-30 19:28:05 +0100

    Added refresh token for API call

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-import axios, {​​ AxiosResponse }​​ from "axios";
+import axios, {​​ AxiosRequestConfig, AxiosResponse }​​ from "axios";
 import {​​ ApiConfig }​​ from "../config/api.config";
 
 export default function api(
@@ -6,10 +6,8 @@ export default function api(
     method: 'get' | 'post' | 'patch' | 'delete',
     body: any | undefined,
 ) {​​
-
-    return new Promise((resolve, reject) => {​​
-
-        axios({​​
+    return new Promise((resolve) => {​​
+        const requestData = {
             method: method,
             url: path,
             baseURL: ApiConfig.API_URL,
@@ -17,12 +15,71 @@ export default function api(
             headers: {​​
                 'Content-Type': 'application/json',
                 'Authorization': getToken(),
-            }​​
-        }​​)
-        .then(res => responseHandler(res, resolve, reject))
-        .catch(err => reject(err));
+            }​​,
+        };
+
+        axios(requestData)
+        .then(res => responseHandler(res, resolve, requestData))
+        .catch(err => {
+            const response: ApiResponse = {
+                status: 'error',
+                data: err
+            };
+            resolve(response);
+        });
     }​​);
-}​​
+}
+
+interface ApiResponse {
+    status: 'ok' | 'error' | 'login';
+    data: any;
+}
+
+async function responseHandler(
+    res: AxiosResponse<any>,
+    resolve: (value?: ApiResponse) => void,
+    requestData: AxiosRequestConfig,
+) {​​
+    if (res.status < 200 || res.status >= 300) {​​
+        if (res.status === 401) {
+            const newToken = await refreshToken(requestData);
+        
+            if(!newToken) {
+                const response: ApiResponse = {
+                    status: 'login',
+                    data: null,
+                };
+                
+                return resolve(response);
+            }​​
+
+            saveToken(newToken);
+
+            requestData.headers['Authorization'] = getToken();
+        
+            return await repeatRequest(requestData, resolve);
+        }
+
+        const response: ApiResponse = {
+            status: 'error',
+            data: res.data,
+        };
+        
+        return resolve(res.data);
+    }​​
+
+
+    if (res.data.statusCode < 0) {​​
+        const response: ApiResponse = {
+            status: 'ok',
+            data: res.data,
+        };
+
+        return resolve(res.data);
+    }​​
+
+    resolve(res.data);
+}
 
 function getToken(): string {​​
     const token = localStorage.getItem('api_token');
@@ -34,24 +91,63 @@ function saveToken(token: string) {​​
 
 }​​
 
-function responseHandler(
-    res: AxiosResponse<any>,
-    resolve: (value: unknown) => void, 
-    reject: (reason?: any) => void
+function getRefreshToken(): string {
+    const token = localStorage.getItem('api_refreshn_token');
+    return token + '';
+}
 
-) {​​
+async function refreshToken(
+    requestData: AxiosRequestConfig
+): Promise<string | null> {
+    const path = 'user/refresh';
+    const data = {
+        token: getRefreshToken(),
+    }
 
-    if (res.status < 200 || res.status >= 300) {​​
-        // 401 - bad token
-        // todo: refresh token and try again
-        // can't refresh token -> redirect user to login
-        return reject(res.data);
+    const refreshTokenRequestData: AxiosRequestConfig = {
+        method: 'post',
+        url: 'path',
+        baseURL: ApiConfig.API_URL,
+        data: JSON.stringify(data),
+        headers: {​​
+            'Content-Type': 'application/json',
+        }​​,
+    };
+    const rtr: { data: { token: string | undefined} } = await axios(refreshTokenRequestData);
 
-    }​​
+    if (!rtr.data.token) {
+        return null;
+    }
 
-    if (res.data.statusCode < 0) {​​
-        return reject(res.data);
-    }​​
+    return rtr.data.token;
+}
 
-    resolve(res.data);
+async function repeatRequest(
+    requestData: AxiosRequestConfig, 
+    resolve: (value?: ApiResponse) => void
+) {
+    axios(requestData)
+    .then(res => {
+        let response: ApiResponse;
+
+        if(res.status === 401) {
+            response = {
+                status: 'login',
+                data: null,
+            };      
+        } else {
+            response = {
+                status: 'ok',
+                data: res,
+            };
+        }    
+        return resolve(response);
+    })
+    .catch(err => {
+        const response: ApiResponse = {
+            status: 'error',
+            data: err,
+        };    
+        return resolve(response);
+    });
 }
